f992905a4977d53372dbc1b8fcd03b70ef225eab
bandrewfisher <bf6038@gmail.com>
2018-10-31 01:01:57 +0000

    Working

--- chatAngular.js
+++ chatAngular.js
@@ -2,10 +2,7 @@ var chatApp = angular.module('chatApp', []);
 chatApp.controller('chatCtrl', function($scope, $http) {
 
     $scope.addMessage = function($event) {
-        $scope.messages.push({
-            content: $scope.messageContent,
-            time: "2:26pm"
-        })
+        console.log("hello");
     }
 });
 
--- index.js
+++ index.js
@@ -3,13 +3,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var messages = {
+  data: []
+}
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/messages', function(req, res) {
+  res.send(messages);
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    messages.data.push(msg);
   });
 });
 
